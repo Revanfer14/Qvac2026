ab4fade984b9523ba25b5847b7c7c9486e1c1c3f
Revanfer14 <revancrozz@gmail.com>
2026-06-14 16:42:13 +0700

    init project

---
+++ qvac2026/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  qvac2026
+//
+//  Created by Revan Ferdinand on 14/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ qvac2026/qvac2026App.swift
@@ -0,0 +1,17 @@
+//
+//  qvac2026App.swift
+//  qvac2026
+//
+//  Created by Revan Ferdinand on 14/06/26.
+//
+
+import SwiftUI
+
+@main
+struct qvac2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
